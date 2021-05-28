bc7c1492d3add82047962760d91f5873ca95e9d3
blackefr <repak120@inbox.ru>
2021-05-28 21:44:21 +0300

    modified

--- topPanel/slideMenu/links/slideMenuLinks.js
+++ topPanel/slideMenu/links/slideMenuLinks.js
@@ -1,13 +1,14 @@
-import { divEl, slideLinks } from "../../../storage/storage.js";
-// import { sliderColumn1, sliderColumn2, sliderColumn3 } from "../slideMenu.js";
+import { divEl, slideLinks, textAt } from "../../../storage/storage.js";
 
 export const link1 = document.createElement(divEl);
 link1.className = slideLinks
-link1.textContent = 'ljdfbihbg'
+link1.textContent = 'первая ссылка'
 
 export const link2 = document.createElement(divEl);
 link2.className = slideLinks
+link2.textContent = 'вторая ссылка'
 export const link3 = document.createElement(divEl);
 link3.className = slideLinks
+link3.textContent = 'третья ссылка'
 
 
